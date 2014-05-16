c3893f1bca64942cbb0dd682bb43162b3f4eb4f2
pahanini <pahanini@gmail.com>
2014-05-16 09:41:11 +0600

    add sphinx match refiner

---
+++ SphinxMatch.php
@@ -0,0 +1,30 @@
+<?php
+namespace pahanini\refiner;
+
+use Yii;
+
+/**
+ * SphinxMatch refiner
+ * @author Pavel Tetyaev <pahanini@gmail.com>
+ */
+class SphinxMatch extends Base
+{
+	private $_params;
+
+	public function init()
+	{
+		parent::init();
+		$this->refine = [$this, 'refine'];
+	}
+
+	public function refine($query, $params)
+	{
+		$this->_params = $params;
+		return $query->match($params);
+	}
+
+	public function getValue()
+	{
+		return $this->_params;
+	}
+}
